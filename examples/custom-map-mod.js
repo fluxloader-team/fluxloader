4d74a6334c533d7b9da8818d1d10016dc968b6a6
tomster12 <thomas.burniston@gmail.com>
2025-02-12 20:54:54 +0000

    Add error checking to custom map example mod

--- examples/custom-map-mod.js
+++ examples/custom-map-mod.js
@@ -5,8 +5,20 @@ exports.modinfo = {
   modauthor: "tomster12",
 };
 
+function checkMapsValid() {
+  if (globalThis.customMapsValid) return true;
+  let valid = true;
+  valid &= globalThis.fs.existsSync(mapPath = globalThis.resolvePathRelativeToExecutable("mods/custom-map/map_blueprint_playtest.png"));
+  valid &= globalThis.fs.existsSync(mapPath = globalThis.resolvePathRelativeToExecutable("mods/custom-map/map_blueprint_playtest_authorization.png"));
+  valid &= globalThis.fs.existsSync(mapPath = globalThis.resolvePathRelativeToExecutable("mods/custom-map/map_blueprint_playtest_lights.png"));
+  valid &= globalThis.fs.existsSync(mapPath = globalThis.resolvePathRelativeToExecutable("mods/custom-map/map_blueprint_playtest_sensors.png"));
+  valid &= globalThis.fs.existsSync(mapPath = globalThis.resolvePathRelativeToExecutable("mods/custom-map/fog_playtest.png"));
+  if (!valid) throw new Error("Custom map files are missing from mods/custom-map/. Please make sure to place the custom map files in the correct location.");
+  globalThis.customMapsValid = valid;
+}
+
 function redirect(path) {
-  console.log(`Redirecting map image file request: ${path}`);
+  checkMapsValid();
   const newPath = globalThis.resolvePathRelativeToExecutable(`mods/custom-map/${path}.png`);
   return { body: globalThis.fs.readFileSync(newPath).toString("base64"), contentType: "image/png" };
 }
