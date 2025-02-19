03621749ae816bcb8468377609ea54bcc48088ca
electric131 <39623923+Electric131@users.noreply.github.com>
2025-02-19 16:33:54 -0600

    Moved subtitle to assets

--- index.js
+++ index.js
@@ -870,132 +870,6 @@ async function fetchJSONWithRetry(url, retries = 200, delay = 100) {
   throw new Error(`Failed to fetch JSON from ${url} after ${retries} retries.`);
 }
 
-moddedSubtitle = function (spawnSolid) {
-	if (globalThis.moddedSubtitleActivePositions && globalThis.moddedSubtitleActivePositions.length == 0) return;
-	const title = [
-		[1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-		[1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-		[1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-		[1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-		[1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-		[1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-		[1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-		[1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-		[1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-		[1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1],
-		[1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-		[1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-	];
-	function tryGet(x, y) {
-		if (x < 0 || y < 0 || y >= title.length || x >= title[y].length) return false;
-		return title[y][x];
-	}
-	const origin = {
-		x: 392 + 80,
-		y: 404 + 32,
-	};
-	const particle = 14;
-	if (!globalThis.moddedSubtitleActivePositions || !globalThis.moddedSubtitleInvalidPositions) {
-		globalThis.moddedSubtitleActivePositions = [
-			{
-				x: 0,
-				y: 0,
-			},
-		];
-		globalThis.moddedSubtitleInvalidPositions = [];
-	}
-	const jumps = {
-		"3, 3": {
-			x: 4,
-			y: 4,
-		},
-		"5, 4": {
-			x: 6,
-			y: 3,
-		},
-		"9, 5": {
-			x: 11,
-			y: 5,
-		},
-		"12, 5": {
-			x: 13,
-			y: 4,
-		},
-		"12, 9": {
-			x: 13,
-			y: 10,
-		},
-		"16, 4": {
-			x: 17,
-			y: 5,
-		},
-		"16, 10": {
-			x: 17,
-			y: 9,
-		},
-		"18, 9": {
-			x: 20,
-			y: 9,
-		},
-		"21, 5": {
-			x: 22,
-			y: 4,
-		},
-		"27, 8": {
-			x: 29,
-			y: 8,
-		},
-		"30, 5": {
-			x: 31,
-			y: 4,
-		},
-		"36, 5": {
-			x: 38,
-			y: 5,
-		},
-		"39, 5": {
-			x: 40,
-			y: 4,
-		},
-		"39, 9": {
-			x: 40,
-			y: 10,
-		},
-		"45, 9": {
-			x: 47,
-			y: 9,
-		},
-		"48, 5": {
-			x: 49,
-			y: 4,
-		},
-	};
-	const offsets = [
-		[-1, 0],
-		[0, 1],
-		[1, 0],
-		[0, -1],
-	];
-	for (const position of [...globalThis.moddedSubtitleActivePositions]) {
-		const posName = `${position.x}, ${position.y}`;
-		spawnSolid(gameInstance.state, origin.x + position.x, origin.y + position.y, particle);
-		const mappedNames = globalThis.moddedSubtitleActivePositions.map((pos) => `${pos.x}, ${pos.y}`);
-		globalThis.moddedSubtitleInvalidPositions.push(posName);
-		globalThis.moddedSubtitleActivePositions.splice(mappedNames.indexOf(posName), 1);
-		if (jumps[posName]) {
-			globalThis.moddedSubtitleActivePositions.push(jumps[posName]);
-		}
-		for (const offset of offsets) {
-			const newPos = { x: position.x + offset[0], y: position.y + offset[1] };
-			const newPosName = `${newPos.x}, ${newPos.y}`;
-			const mappedNames = globalThis.moddedSubtitleActivePositions.map((pos) => `${pos.x}, ${pos.y}`);
-			if (tryGet(newPos.x, newPos.y) && !globalThis.moddedSubtitleInvalidPositions.includes(newPosName) && !mappedNames.includes(newPosName)) {
-				globalThis.moddedSubtitleActivePositions.push(newPos);
-			}
-		}
-	}
-};
-
 async function finalizeModloaderPatches() {
   if (!globalThis.config.debug.enableDebugMenu) {
     globalThis.bundlePatches.push({
@@ -1009,7 +883,7 @@ async function finalizeModloaderPatches() {
     globalThis.bundlePatches.push({
       type: "regex",
       pattern: "if\\(t\\.store\\.scene\\.active===x\\.MainMenu\\)(.+?)else",
-      replace: `if(t.store.scene.active===x.MainMenu){setInterval(${moddedSubtitle.toString().replace(/[\n\t]+/g, '')}, 60, Fd);$1}else`,
+      replace: `if(t.store.scene.active===x.MainMenu){setInterval(globalThis.moddedSubtitle, 60, Fd);$1}else`,
       expectedMatches: 1,
     });
   }
