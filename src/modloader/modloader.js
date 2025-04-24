b2b7b47b03fe96b2825236cd16252cea39e8026b
tomster12 <thomas.burniston@gmail.com>
2025-04-24 21:00:09 +0100

    Sorting working, going to remove

--- src/modloader/modloader.js
+++ src/modloader/modloader.js
@@ -30,6 +30,10 @@ function handleResizer(resizer) {
 		document.addEventListener("mouseup", onMouseUp);
 	});
 
+	resizer.addEventListener("click", (e) => {
+		e.stopPropagation();
+	});
+
 	function onMouseMove(e) {
 		const newWidth = startWidth + (e.pageX - startX) * (isLeft ? -1 : 1);
 		parent.style.width = newWidth + "px";
@@ -107,40 +111,51 @@ function selectTab(tab) {
 
 class ModsTab {
 	columns = {};
-	rows = [];
-	selectedMod = -1;
+	modRows = {};
+	defaultOrder = [];
+	selectedMod = null;
+	sortingColumn = null;
 
 	setup() {
 		getElement("mods-tab-table")
 			.querySelectorAll("th")
 			.forEach((element) => {
 				const column = element.getAttribute("data-column");
-				this.columns[column] = { element };
+				this.columns[column] = { element, sortingType: 0 };
+				element.addEventListener("click", () => {
+					this.selectMod(null);
+					this.clickColumn(column);
+				});
 			});
 	}
 
 	select() {
+		if (this.sortingColumn) this.unselectSortingColumn();
+
 		electron.invoke("ml-modloader:get-mods").then((mods) => {
 			this.setMods(mods);
 		});
 	}
 
-	deselect() {}
+	deselect() {
+		// TODO
+	}
 
 	setMods(mods) {
 		const tbody = getElement("mods-tab-table").querySelector("tbody");
 		tbody.innerHTML = "";
-		this.rows = [];
+		this.modRows = {};
+		this.defaultOrder = mods.map((mod) => mod.info.modID);
 		let index = 0;
 		for (const mod of mods) {
-			const element = this.createModRowElement(mod, index);
-			tbody.appendChild(element);
-			this.rows.push({ element, mod });
+			const row = this.createModRow(mod, index);
+			this.modRows[mod.info.modID] = row;
+			tbody.appendChild(row.element);
 			index += 1;
 		}
 	}
 
-	createModRowElement(mod, index) {
+	createModRow(mod, index) {
 		const element = createElement(`
 			<tr>
 				<td><input type="checkbox" ${mod.isEnabled ? "checked" : ""}></td>
@@ -164,8 +179,8 @@ class ModsTab {
 		element.classList.toggle("disabled", !mod.isEnabled);
 
 		element.querySelector("input").addEventListener("click", (e) => e.stopPropagation());
-		
-		element.addEventListener("click", (e) => this.selectMod(index));
+
+		element.addEventListener("click", (e) => this.selectMod(mod.info.modID));
 
 		element.querySelector("input").addEventListener("change", (e) => {
 			const checkbox = e.target;
@@ -180,58 +195,138 @@ class ModsTab {
 			});
 		});
 
-		return element;
+		return { element, mod, curentIndex: index, isVisible: true };
 	}
 
-	selectMod(index) {
-		if (this.selectedMod === index) {
-			this.rows[index].element.classList.remove("selected");
+	selectMod(modID) {
+		if (this.selectedMod !== null && this.selectedMod === modID) {
+			this.modRows[this.selectedMod].element.classList.remove("selected");
 			this.setModInfo(null);
-			this.selectedMod = -1;
+			this.selectedMod = null;
 			return;
 		}
-		if (this.selectedMod !== -1) {
-			this.rows[this.selectedMod].element.classList.remove("selected");
+		if (this.selectedMod !== null) {
+			this.modRows[this.selectedMod].element.classList.remove("selected");
 		}
-		this.selectedMod = index;
-		this.rows[index].element.classList.add("selected");
-		this.setModInfo(this.rows[index].mod);
+		if (modID != null) {
+			this.selectedMod = modID;
+			this.modRows[modID].element.classList.add("selected");
+			this.setModInfo(modID);
+		}
 	}
 
-	setModInfo(mod) {
+	setModInfo(modID) {
+		const mod = this.modRows[modID].mod;
+
 		if (mod == null) {
 			getElement("mod-info").style.display = "none";
 			getElement("mod-info-empty").style.display = "block";
+			return;
+		}
+
+		getElement("mod-info").style.display = "block";
+		getElement("mod-info-empty").style.display = "none";
+
+		getElement("mod-info-title").innerText = mod.info.name;
+
+		getElement("mod-info-description").classList.toggle("empty", mod.info.description ? mod.info.description.length === 0 : true);
+		if (mod.info.description && mod.info.description.length > 0) {
+			getElement("mod-info-description").innerText = mod.info.description;
 		} else {
-			getElement("mod-info").style.display = "block";
-			getElement("mod-info-empty").style.display = "none";
+			getElement("mod-info-description").innerText = "No description provided.";
+		}
 
-			getElement("mod-info-title").innerText = mod.info.name;
+		getElement("mod-info-mod-id").innerText = mod.info.modID;
+		getElement("mod-info-author").innerText = mod.info.author;
+		getElement("mod-info-version").innerText = mod.info.version;
+		getElement("mod-info-last-updated").innerText = mod.info.lastUpdated;
 
-			getElement("mod-info-description").classList.toggle("empty", mod.info.description ? mod.info.description.length === 0 : true);
-			if (mod.info.description && mod.info.description.length > 0) {
-				getElement("mod-info-description").innerText = mod.info.description;
+		if (mod.info.tags) {
+			getElement("mod-info-tags").classList.toggle("empty", mod.info.tags.length === 0);
+			if (mod.info.tags.length === 0) {
+				getElement("mod-info-tags").innerText = "No tags provided.";
 			} else {
-				getElement("mod-info-description").innerText = "No description provided.";
-			}
-
-			getElement("mod-info-mod-id").innerText = mod.info.modID;
-			getElement("mod-info-author").innerText = mod.info.author;
-			getElement("mod-info-version").innerText = mod.info.version;
-			getElement("mod-info-last-updated").innerText = mod.info.lastUpdated;
-
-			if (mod.info.tags) {
-				getElement("mod-info-tags").classList.toggle("empty", mod.info.tags.length === 0);
-				if (mod.info.tags.length === 0) {
-					getElement("mod-info-tags").innerText = "No tags provided.";
-				} else {
-					getElement("mod-info-tags").innerHTML = "";
-					for (const tag of mod.info.tags) {
-						const tagElement = createElement(`<span class="tag">${tag}</span>`);
-						getElement("mod-info-tags").appendChild(tagElement);
-					}
+				getElement("mod-info-tags").innerHTML = "";
+				for (const tag of mod.info.tags) {
+					const tagElement = createElement(`<span class="tag">${tag}</span>`);
+					getElement("mod-info-tags").appendChild(tagElement);
 				}
 			}
+		}
+	}
+
+	onSearchChanged() {
+		// TODO
+	}
+
+	onSelectedTagsChanged() {
+		// TODO
+	}
+
+	updateFilteredMods() {
+		// TODO
+	}
+
+	clickColumn(column) {
+		this.selectSortingColumn(column);
+	}
+
+	unselectSortingColumn() {
+		if (!this.sortingColumn) return;
+		this.columns[this.sortingColumn].sortingType = 0;
+		this.columns[this.sortingColumn].element.classList.remove("ascending");
+		this.columns[this.sortingColumn].element.classList.remove("descending");
+	}
+
+	selectSortingColumn(column) {
+		console.log("Sorting mods by column:", column);
+		if (this.sortingColumn != column) this.unselectSortingColumn();
+
+		let rows = [];
+
+		// 2 -> 0: Descending -> None
+		if (this.columns[column].sortingType === 2) {
+			for (const modID of this.defaultOrder) rows.push(this.modRows[modID]);
+			this.columns[column].sortingType = 0;
+			this.columns[column].element.classList.remove("ascending");
+			this.columns[column].element.classList.remove("descending");
+			this.sortingColumn = null;
+		}
+
+		// 0 -> 1 -> 2: None -> Ascending -> Descending
+		else {
+			let comparator;
+			switch (column) {
+				case "name":
+					comparator = (a, b) => a.mod.info.name.localeCompare(b.mod.info.name);
+					break;
+				case "version":
+					comparator = (a, b) => a.mod.info.version.localeCompare(b.mod.info.version);
+					break;
+				case "author":
+					comparator = (a, b) => a.mod.info.author.localeCompare(b.mod.info.author);
+					break;
+				default:
+					console.warn("Unknown column:", column);
+					return;
+			}
+
+			const ascending = this.columns[column].sortingType === 0;
+			rows = Object.values(this.modRows);
+			rows.sort(comparator);
+			if (!ascending) rows.reverse();
+			this.columns[column].sortingType = ascending ? 1 : 2;
+			this.columns[column].element.classList.toggle("ascending", ascending);
+			this.columns[column].element.classList.toggle("descending", !ascending);
+			this.sortingColumn = column;
+		}
+
+		// Update tbody with sorted rows
+		const tbody = getElement("mods-tab-table").querySelector("tbody");
+		tbody.innerHTML = "";
+		for (let i = 0; i < rows.length; i++) {
+			tbody.appendChild(rows[i].element);
+			this.modRows[rows[i].mod.info.modID].curentIndex = i;
 		}
 	}
 }
