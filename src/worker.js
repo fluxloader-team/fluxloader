6e29f0df9446217a4133bba6881f122c2854d717
tomster12 <thomas.burniston@gmail.com>
2025-10-04 20:31:34 +0100

    Update to 2.2.3

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,7 @@
 // ------------- VARIABLES -------------
 
 // -- CHANGE VERSION SEARCH : Search this to find where to change version
-globalThis.fluxloaderVersion = "2.2.2";
+globalThis.fluxloaderVersion = "2.2.3";
 globalThis.fluxloaderAPI = undefined;
 
 // ------------- UTILTY -------------
