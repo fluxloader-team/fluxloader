3627998fe649566f536cd37f10884b41f79e69ee
tomster12 <thomas.burniston@gmail.com>
2025-10-20 21:51:21 +0100

    Increment version

--- src/electron.js
+++ src/electron.js
@@ -29,7 +29,7 @@ import Module from "module";
 
 // =================== VARIABLES ===================
 
-globalThis.fluxloaderVersion = "2.2.6";
+globalThis.fluxloaderVersion = "2.2.7";
 globalThis.fluxloaderAPI = undefined;
 globalThis.gameElectronFuncs = undefined;
 globalThis.gameWindow = undefined;
